90250f196d8fb32829e3080546bd5bb6d9a7933b
Bret Hogg <hoggb@wwu.edu>
2017-11-06 20:17:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function Dog(name, breed, age){
   this.breed = breed;
   this.age = age;
 }
+
+function Cat(name, breed, age){
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+}
+
+function Ferret(name, age, foodRequirements){
+  this.name = name;
+  this.age = age.
+  this.foodRequirements = foodRequirements;
+}
